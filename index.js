15b704296e5979013d10a98e8c92cf4073da9945
gokhanDerala <gderala@gmail.com>
2018-12-07 00:01:07 +0300

    changes

--- index.js
+++ index.js
@@ -42,7 +42,28 @@ function turn(squareId, player){
 }
 
 function checkWin(board, player){
-	
+	let plays = board.reduce((a, e, i) => (e === player) ? a.concat(i) : a, []);
+	let gameWon = null;
+
+	for (let [index, win] of winningCombination.entries()) {
+		if(win.every(elem => plays.indexOf(elem) > -1)) {
+			gameWon = {index: index, player: player};
+			break;
+		}
+	}
+
+	return gameWon;
+}
+
+function gameOver(gameWon){
+	for (let index of winningCombination[gameWon.index]) {
+		document.getElementById(index).style.backgroundColor =
+			gameWon.player == humanPlayer ? "blue" : "red";
+	}
+
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].removeEventListener('click',turnClick, false);
+	}
 }
 
 
